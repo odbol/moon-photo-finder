71d824054ee139fbe14dedd14c813746006f6cbc
Tyler Freeman <git@odbol.com>
2020-05-17 14:43:42 -0700

    Got moon sun table printing

--- src/app/moonsun/moonsun.component.ts
+++ src/app/moonsun/moonsun.component.ts
@@ -1,52 +1,84 @@
 import { Component, OnInit } from '@angular/core';
 
-import { Observable } from 'rxjs';
-import { filter, map, flatMap } from 'rxjs/operators';
-
-import SunCalc from "suncalc";
+import SunCalc from 'suncalc';
 import { GeolocationService } from '../geolocation.service';
 
 const MS_PER_DAY = 1000 * 60 * 60 * 24;
 
+interface SunMoonDay {
+  date: Date;
+  moontimes: {
+    rise: Date;
+    set: Date;
+  };
+  moonPhase: {
+    fraction: number;
+    phase: number;
+    angle: number;
+  };
+  suntimes: {
+    solarNoon: Date;
+    nadir: Date;
+    sunrise: Date;
+    sunset: Date;
+    sunriseEnd: Date;
+    sunsetStart: Date;
+    dawn: Date;
+    dusk: Date;
+    nauticalDawn: Date;
+    nauticalDusk: Date;
+    nightEnd: Date;
+    night: Date;
+    goldenHourEnd: Date;
+    goldenHour: Date;
+  };
+}
+
 @Component({
   selector: 'app-moonsun',
   templateUrl: './moonsun.component.html',
-  styleUrls: ['./moonsun.component.scss']
+  styleUrls: ['./moonsun.component.scss'],
 })
 export class MoonsunComponent implements OnInit {
-
-  times: Observable<any>;
+  times: SunMoonDay[] | undefined;
 
   numDays = 365;
 
-  error: string|undefined;
+  error: string | undefined;
 
   columnsToDisplay = ['date', 'phase'];
 
   constructor(private geolocationService: GeolocationService) {}
 
   ngOnInit(): void {
-    this.times = this.geolocationService.getCurrentPosition()
-      .pipe(flatMap(this.onLocationUpdate));
+    this.geolocationService.getCurrentPosition().subscribe({
+      next: this.onLocationUpdate.bind(this),
+      error: this.onError.bind(this),
+    });
   }
 
-  onLocationUpdate(position: Position): Observable<any> {
-    return new Observable((observer) => {
-      console.log('onLocationUpdate', position);
+  onLocationUpdate(position: Position) {
+    console.log('onLocationUpdate', position);
 
-      const coords = position.coords;
-      const curTimeMs = Date.now();
-      for (let i = 0; i < this.numDays; i++) {
-        const date = new Date(curTimeMs + i * MS_PER_DAY);
-        observer.next({
+    const coords = position.coords;
+    const curTimeMs = Date.now();
+    const result = [];
+    for (let i = 0; i < this.numDays; i++) {
+      const date = new Date(curTimeMs + i * MS_PER_DAY);
+      result.push({
+        date,
+        moontimes: SunCalc.getMoonTimes(
           date,
-          moontimes: SunCalc.getMoonTimes(date, coords.latitude, coords.longitude),
-          moonPhase: SunCalc.getMoonIllumination(date),
-          suntimes: SunCalc.getTimes(date, coords.latitude, coords.longitude)
-        });
-      }
+          coords.latitude,
+          coords.longitude
+        ),
+        moonPhase: SunCalc.getMoonIllumination(date),
+        suntimes: SunCalc.getTimes(date, coords.latitude, coords.longitude),
+      });
+    }
 
-    });
+    console.log('onLocationUpdate', result);
+    this.times = result;
   }
 
   onError(error: Error) {
